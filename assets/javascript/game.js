33f680c21d15349bf1fd6d1a2c4e9a1acee1576a
Jean Peel <Jean.Peel.246@gmail.com>
2020-01-15 04:03:49 -0700

    working on wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,16 @@
-var characters = ["a","b","c"]
-var randomLetter = Math.floor(Math.random() * 100);  
-var userText = document.getElementById("user-text")
+var characters = ["a","b","c"];
+var randomNumber = Math.floor(Math.random() * 100);
+
+var wins = 0;
+var losses = 0;
+
+var userText = document.getElementById("user-text");
+var computerText = document.getElementById("computer-text");
+
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById ("losses-text");
+
+
 
 for (var i = 0; i < characters.length; i++ ) {
     console.log ("ilist " + characters[i])
@@ -21,11 +31,22 @@ for (var l = 0; l < 36; l++) {
 
     
 console.log("length " + characters.length);
-console.log("location " + characters[1])
-console.log("random " + randomLetter)
+console.log("location " + characters[1]);
+console.log("random " + randomNumber);
 
-document.write("random " + randomLetter)
+document.write("random " + randomNumber);
+
 
 document.onkeyup = function(event) {
     userText.textContent = event.key;
-}
+    var userGuess = event.key;
+    var computerGuess = characters[Math.foor(Math.random() * characters.length)]
+
+    // if (userGuess === computerGuess) {
+    //     wins++;
+    // } else (userGuess != computerGuess) {
+    //     looses++;
+    // }
+
+    // document.write("computer " + computerGuess);
+};
