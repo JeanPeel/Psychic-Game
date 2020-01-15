2c599895540e4cc168a8506f57dcf8412e0cda38
Jean Peel <Jean.Peel.246@gmail.com>
2020-01-15 02:20:04 -0700

    made random number generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,6 @@
-// function makeid(length) {
-//     var result = '';
-//     var characters = 'abcdefghijklmnopqrstuvwxyz';
-//     var charactersLength = characters.length;
-//     for ( var i=0; i < length, i++) {
-//         result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//     }
-//     return result;
-// }
-
-// console.log(makeid(5));
-
-// Math.random().toString(36).replace(/[^a-z]+/g, '')
-
-let r = Math.random().toString(36).substring(12);
-console.log("random", r);
+var characters = ["a","b","c"]
+var randomLetter = Math.floor(Math.random() * 100);    
+    
+console.log(characters.length);
+console.log(characters[1])
+console.log(randomLetter)
