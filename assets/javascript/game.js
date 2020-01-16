ffdc1b33f948bd42c0c7e27d1d5eb0cf0cc9625e
Jean Peel <Jean.Peel.246@gmail.com>
2020-01-15 23:44:15 -0700

    got wins losses guesses and rounds to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,94 @@
-var characters = ["a","b","c"];
-var randomNumber = Math.floor(Math.random() * 100);
+// changes to the page
+var winsTxt = document.getElementById("wins-txt");
+var lossesTxt = document.getElementById ("losses-txt");
+var roundsTxt = document.getElementById ("rounds-txt");
+var guessesTxt = document.getElementById ("guesses-txt");
+var userTxt = document.getElementById("user-txt");
+
+// array of choices for the computer
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+];
 
 var wins = 0;
 var losses = 0;
+var rounds = 10;
+var guesses = 0;
 
-var userText = document.getElementById("user-text");
-var computerText = document.getElementById("computer-text");
-
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById ("losses-text");
-
-
-
-for (var i = 0; i < characters.length; i++ ) {
-    console.log ("ilist " + characters[i])
-}
-
-for (var j = 0; j < characters.length; j++ ) {
-    console.log ("jlist " + characters[j])
-}
-
-for (var k = 0; k < 5; k++) {
-    console.log("I am " + k);
-}
-
-for (var l = 0; l < 36; l++) {
-    console.log("l I am " + l);
-}
-
-
-    
-console.log("length " + characters.length);
-console.log("location " + characters[1]);
-console.log("random " + randomNumber);
-
-document.write("random " + randomNumber);
-
-
+// Begin game on key up
 document.onkeyup = function(event) {
-    userText.textContent = event.key;
+    
+    // Users guesses recorded and reported to html
     var userGuess = event.key;
-    var computerGuess = characters[Math.foor(Math.random() * characters.length)]
+    userTxt.textContent = event.key;
 
-    // if (userGuess === computerGuess) {
-    //     wins++;
-    // } else (userGuess != computerGuess) {
-    //     looses++;
-    // }
+    // Computer chooses a random letter
+    var randomLet = Math.floor(Math.random() * letters.length);
+    var computerLetter = letters[randomLet];
+    
+    // Variables created to adjust score
+    // Adjusted scores reported to html
+    var youWon = wins;  
+    winsTxt.textContent = youWon;
 
-    // document.write("computer " + computerGuess);
-};
+    var youLosse = losses;
+    lossesTxt.textContent = youLosse;
+
+    var looseRound = rounds;
+    roundsTxt.textContent = looseRound;
+
+    var youGuessed = guesses;
+    guessesTxt.textContent = youGuessed;
+
+    // User gets a point if their guess is equal to the computers
+    if (userGuess === computerLetter) {
+        wins++;  
+    }
+
+    // If the user looses 10 times they loose a round, 
+    // lost round is recorded and losses resets.
+    if  (losses > 9) {
+        rounds --;   
+        losses = 0
+    }
+   
+    // losses and guesses goes if if looser does not win
+    else {
+        losses ++;
+        guesses++;      
+    }
+
+    if (rounds < 0) {
+        var confirmReload = alert("Do you want to reset the game?");
+        confirmReload
+        location.reload();
+    }
+
+    if (confirmReload) {
+        wins = 0;
+        losses = 0;
+        rounds = 10;
+        guesses = 0;
+        
+    }
+
+    console.log(computerLetter)
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
+
+// document.write("random " + randomNumber);
