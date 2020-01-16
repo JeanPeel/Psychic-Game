e1c4375f54aab9714eb556f097aa68458bc04912
Jean Peel <Jean.Peel.246@gmail.com>
2020-01-16 00:27:47 -0700

    finalized layout and notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,29 +5,30 @@ var roundsTxt = document.getElementById ("rounds-txt");
 var guessesTxt = document.getElementById ("guesses-txt");
 var userTxt = document.getElementById("user-txt");
 
-// array of choices for the computer
+// array of choices for the computer.
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
 ];
 
+// Score. 
 var wins = 0;
 var losses = 0;
-var rounds = 10;
+var rounds = 25;
 var guesses = 0;
 
-// Begin game on key up
+// Begin game on key up.
 document.onkeyup = function(event) {
     
-    // Users guesses recorded and reported to html
-    var userGuess = event.key;
+    // Users guesses recorded and reported to html.
+    var userGuess = event.key.toLowerCase();
     userTxt.textContent = event.key;
 
-    // Computer chooses a random letter
+    // Computer chooses a random letter.
     var randomLet = Math.floor(Math.random() * letters.length);
     var computerLetter = letters[randomLet];
     
-    // Variables created to adjust score
-    // Adjusted scores reported to html
+    // Variables created to adjust score.
+    // Adjusted scores reported to html.
     var youWon = wins;  
     winsTxt.textContent = youWon;
 
@@ -40,7 +41,7 @@ document.onkeyup = function(event) {
     var youGuessed = guesses;
     guessesTxt.textContent = youGuessed;
 
-    // User gets a point if their guess is equal to the computers
+    // User gets a point if their guess is equal to the computers.
     if (userGuess === computerLetter) {
         wins++;  
     }
@@ -52,43 +53,45 @@ document.onkeyup = function(event) {
         losses = 0
     }
    
-    // losses and guesses goes if if looser does not win
+    // losses and guesses goes up if looser does not win.
     else {
         losses ++;
         guesses++;      
     }
 
+    // when rounds runs out user is alerted that they loose,
+    // page reloads.
     if (rounds < 0) {
-        var confirmReload = alert("Do you want to reset the game?");
-        confirmReload
+        var confirmLost = alert("You Loose! Do you want to reset the game?");
+        confirmLost
         location.reload();
     }
 
-    if (confirmReload) {
-        wins = 0;
-        losses = 0;
-        rounds = 10;
-        guesses = 0;
-        
+    // if user get ten wins they are alerted that they win,
+    // page reloads.
+    if (wins === 10) {
+        var confirmWon = alert("You WON 10 times! Do you want to reset the game?")
+        confirmWon
+        location.reload();
     }
 
+    // game resets if lost or won.
+
+    if (confirmLost) {
+        wins = 0;
+        losses = 0;
+        rounds = 25;
+        guesses = 0;  
+    }
+
+    if (confirmWon) {
+        wins = 0;
+        losses = 0;
+        rounds = 25;
+        guesses = 0;  
+    }
+
+    // computer guesses logged.
     console.log(computerLetter)
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-;
-
-// document.write("random " + randomNumber);
